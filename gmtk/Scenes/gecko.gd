2e297b146c5ca8452bef24d42e101085017a7b30
MenacingGhost <101933205+MenacingGhost@users.noreply.github.com>
2024-08-17 23:33:29 +0700

    Added stamina

--- gmtk/Scenes/gecko.gd
+++ gmtk/Scenes/gecko.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 
 var SPEED = 100.0
+var HP = 3
+var STAMINA = 10
 
 signal actionPressed
 
@@ -26,23 +28,36 @@ func _process(delta):
 			ShootState = true
 			SPEED = 0
 			$GunPivotPoint/Gun.show()
+			$Timer.stop()
 			
 		elif ClimbState == false:
 			ClimbState = true
 			ShootState = false
 			SPEED = 100.0
 			$GunPivotPoint/Gun.hide()
+			$Timer.start()
 	
 func _physics_process(delta):
-	if climbable == true:
+	
+	if climbable == true and STAMINA > 0:
 		var direction = Input.get_vector("left", "right" , "up" , "down")
 		if direction:
 			velocity = direction * SPEED
 		else:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
 			velocity.y = move_toward(velocity.y, 0, SPEED)
-	else:
+	if climbable == false or STAMINA <= 0:
 		velocity += get_gravity() * delta
 
 	move_and_slide()
 	
+
+
+func _on_timer_timeout():
+	STAMINA -= 1
+	print(STAMINA)
+
+
+func _on_area_2d_body_entered(body):
+	STAMINA += 5
+	print("+STAMNIA")
